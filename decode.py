7e62cee40f5b70a204a5386c1c417b2eba2de017
JohannUM <72266031+JohannUM@users.noreply.github.com>
2022-10-10 20:11:59 +0200

    Add files via upload

---
+++ decode.py
@@ -0,0 +1,36 @@
+secret_message = input()
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def get_frequencies(msg : str):
+    freq = {}
+    for l in msg:
+        freq[l] = freq.get(l, 0) + 1
+    return freq
+
+def calculate_shift(freq: dict, msg : str):
+    counter = 0
+    highest_freq = None
+    for key in freq:
+        if freq[key] > counter:
+            highest_freq = key
+            counter = freq[key]
+    pos = alphabet.find(highest_freq)
+    if(pos > 4):
+        return (pos - 4)*-1
+    else:
+        return 4 - pos
+
+def decode_msg(shift : int, msg : str):
+    decoded_msg = ''
+    for l in msg:
+        pos = alphabet.find(l)
+        curr_shift = pos + shift
+        if (curr_shift >= len(alphabet)):
+            curr_shift = len(alphabet)-curr_shift-1
+        decoded_msg = decoded_msg + alphabet[curr_shift]
+    print(decoded_msg)
+
+
+s = calculate_shift(get_frequencies(secret_message), secret_message)
+decode_msg(s, secret_message)
+
